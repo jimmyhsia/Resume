855e653c647fbef825d011d39185e29cd4b937e9
jimmyhsia <jimmyhsia0522@gmail.com>
2019-02-20 17:35:44 +0800

    20190220 parallaxy

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <b-navbar type="dark" variant="dark">
+    <b-navbar type="dark" variant="dark" class="indexmenu">
     <b-navbar-nav>
       <b-nav-item href="#">Home</b-nav-item>
-
-      <!-- Navbar dropdowns -->
+      <b-nav-item href="#">About</b-nav-item>
+      <!-- Navbar dropdowns 
       <b-nav-item-dropdown text="Lang" right>
         <b-dropdown-item href="#">EN</b-dropdown-item>
         <b-dropdown-item href="#">ES</b-dropdown-item>
@@ -15,7 +15,7 @@
       <b-nav-item-dropdown text="User" right>
         <b-dropdown-item href="#">Account</b-dropdown-item>
         <b-dropdown-item href="#">Settings</b-dropdown-item>
-      </b-nav-item-dropdown>
+      </b-nav-item-dropdown>-->
     </b-navbar-nav>
   </b-navbar>
 
@@ -25,8 +25,31 @@
     </div> -->
     <!-- <router-view/> -->
   </div>
+
+
+<parallax>
+  <img src="https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg">
+</parallax>
+
+
+
 </template>
 
-<style>
-
+<style lang="scss">
+.indexmenu {
+    position: fixed;
+    width: 100%;
+    z-index: 50;
+    transition: background ease-in-out .2s;
+    background: rgba(0, 0, 0, 0.8);
+}
 </style>
+<script>
+ import Parallax from 'vue-parallaxy'
+ export default {
+  
+  components: {
+    Parallax
+  }
+}
+</script>
